c986f5079693eaaf098369d24062509d712b57a7
Dave Bunyan <davebunyan1@gmail.com>
2026-09-05 11:48:32 -0400

    basic tests added

--- src/routers/endpoints.py
+++ src/routers/endpoints.py
@@ -1,40 +1,42 @@
 from typing import Annotated
 
-from fastapi import Body
+from fastapi import APIRouter, Body
 
-from main import app
+router = APIRouter(prefix="")
 
-fake_jobs = {
+FAKE_JOBS = {
     "001": {"job_type": "first_type", "job_message": "Hello"},
     "002": {"job_type": "second_type", "job_message": "World!"},
 }
 
 
-@app.get("/jobs")
+@router.get("/jobs")
 def get_jobs():
-    return {"jobs": fake_jobs}
+    return {"jobs": FAKE_JOBS}
 
 
-@app.get("/jobs/{job_id}")
+@router.get("/jobs/{job_id}")
 def get_job(job_id: str):
-    return fake_jobs[job_id]
+    print("job_id:", job_id)
+    print("fake_jobs:", FAKE_JOBS)
+    return FAKE_JOBS[job_id]
 
 
-@app.post("/jobs")
+@router.post("/jobs")
 def create_job(job: Annotated[dict[str, dict[str, str]], Body()]):
-    fake_jobs.update(job)
-    return fake_jobs
+    FAKE_JOBS.update(job)
+    return FAKE_JOBS
 
 
-@app.delete("/jobs/{job_id}")
+@router.delete("/jobs/{job_id}")
 def delete_job(job_id: str):
-    return fake_jobs.pop(job_id)
+    return FAKE_JOBS.pop(job_id)
 
 
-@app.put("/jobs/{job_id}")
+@router.put("/jobs/{job_id}")
 def update_job(
     job_id: str,
     job_message: Annotated[str, Body()],
 ):
-    fake_jobs[job_id]["job_message"] = job_message
-    return fake_jobs
+    FAKE_JOBS[job_id]["job_message"] = job_message
+    return FAKE_JOBS
