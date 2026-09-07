1768e895eb175c6358d75c0a29349b06259dc31b
Dave Bunyan <davebunyan1@gmail.com>
2026-09-06 23:38:15 -0400

    feat: concurrent requests added

--- scripts/simulate_users.py
+++ scripts/simulate_users.py
@@ -1,59 +1,60 @@
+import asyncio
 import random
-import time
 
 import httpx2
 
 BASE_URL = "http://127.0.0.1:8000"
 
 
-def simulate_user(user_id: int) -> None:
-    with httpx2.Client(base_url=BASE_URL) as client:
-        action = random.choice(
-            ["get_all", "get_job", "create_job", "update_job", "delete_job"]
+async def simulate_user(user_id: int, client: httpx2.AsyncClient) -> None:
+    action = random.choice(
+        ["get_all", "get_job", "create_job", "update_job", "delete_job"]
+    )
+
+    print(f"User {user_id} performing action {action}")
+
+    if action == "get_all":
+        response = await client.get("/jobs")
+
+    elif action == "get_job":
+        # 001 will always be successful, 002 will be if not deleted, 1000 will always fail
+        job_id = random.choice(["001", "002", "1000"])
+        response = await client.get(f"/jobs/{job_id}")
+    elif action == "create_job":
+        job_id = str(random.randint(100, 999))
+        response = await client.post(
+            "/jobs",
+            json={
+                "job_id": job_id,
+                "job_type": "simulated",
+                "job_message": "Hello",
+            },
         )
 
-        print(f"User {user_id} performing action {action}")
-
-        if action == "get_all":
-            response = client.get("/jobs")
-        elif action == "get_job":
-            # 001 will always be successful, 002 will be if not deleted, 1000 will always fail
-            job_id = random.choice(["001", "002", "1000"])
-            response = client.get(f"/jobs/{job_id}")
-        elif action == "create_job":
-            job_id = str(random.randint(100, 999))
-            response = client.post(
-                "/jobs",
-                json={
-                    "job_id": job_id,
-                    "job_type": "simulated",
-                    "job_message": "Hello",
-                },
-            )
-
-        elif action == "update_job":
-            job_id = random.choice(["001", "002", "1000"])
-            response = client.put(
-                f"/jobs/{job_id}",
-                json="Updated by simulated user",
-            )
-
-        elif action == "delete_job":
-            job_id = random.choice(["002", "1000"])
-            response = client.delete(f"/jobs/{job_id}")
-
-        print(
-            f"User {user_id}: "
-            f"{response.request.method} {response.request.url.path} "  # type: ignore
-            f"→ {response.status_code}"  # type: ignore
+    elif action == "update_job":
+        job_id = random.choice(["001", "002", "1000"])
+        response = await client.put(
+            f"/jobs/{job_id}",
+            json="Updated by simulated user",
         )
 
+    elif action == "delete_job":
+        job_id = random.choice(["002", "1000"])
+        response = await client.delete(f"/jobs/{job_id}")
 
-def main():
-    for user_id in range(1, 11):
-        simulate_user(user_id)
-        time.sleep(random.uniform(0.2, 1.0))
+    print(
+        f"User {user_id}: "
+        f"{response.request.method} {response.request.url.path} "  # type: ignore
+        f"→ {response.status_code}"  # type: ignore
+    )
+
+
+async def main():
+    async with httpx2.AsyncClient(base_url=BASE_URL) as client:
+        users = [simulate_user(user_id, client) for user_id in range(1, 31)]
+
+        await asyncio.gather(*users)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
